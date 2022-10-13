11d7336bd1eed42907ba1db80c0988e45d511e49
Uracha <33252558+kanommoji@users.noreply.github.com>
2022-10-13 16:38:12 +0700

    EDIT: move file main.go and write error is walk out of table

--- src/main.go
+++ src/main.go
@@ -7,9 +7,14 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		walk := r.URL.Query().Get("walk")
-		walkig := robotwalk.RobotWalk(walk)
+		walks := r.URL.Query().Get("walks")
 		w.Header().Set("Content-Type", "application/text")
+		walkig, err := robotwalk.RobotWalk(walks)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
 		w.Write([]byte(walkig))
 	})
 	http.ListenAndServe(":8080", nil)
